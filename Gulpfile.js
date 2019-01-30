96ac91e999122178508d6c17da70a562c0277def
Szalai László <szalai.laszlo@tudaskozpont-pecs.hu>
2019-01-30 11:39:09 +0100

    Composer

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,15 +1,15 @@
 var gulp = require('gulp');
 
-var outputDir = 'assets/summernote';
+// var outputDir = 'assets/summernote';
+//
+// gulp.task('copy-summernote-image-attributes', function() {
+// 	gulp.src('**/*')
+// 		.pipe(gulp.dest(outputDir+'/plugin/image-attributes'))
+// });
 
-gulp.task('copy-summernote', function() {
-	gulp.src('summernote')
-		.pipe(gulp.dest(outputDir))
+gulp.task('copy-summernote-image-attributes', function() {
+	gulp.src('vendor/egonom/summernote-image-attributes/**/*')
+		.pipe(gulp.dest('public/assets/vendor/summernote/plugin/image-attributes'))
 });
 
-gulp.task('copy-plugin', function() {
-	gulp.src('dist/uploadfile.js')
-		.pipe(gulp.dest(outputDir+'/plugin'))
-});
-
-gulp.task('default', ['copy-summernote', 'copy-plugin']);
+gulp.task('default', ['copy-summernote-image-attributes']);
